4bd7503ecd4fea4a5bc98372c2535b1171a2368f
gahee-kim <85275890+gahee-kim@users.noreply.github.com>
2021-10-10 18:06:38 +0900

    Add files via upload

--- lib/extension_api.dart
+++ lib/extension_api.dart
@@ -1,8 +1,9 @@
 library flutter_map_marker_popup.extension_api;
 
-// This was added for flutter_map_marker_cluster to add popup functionality.
-
-export 'src/popup_snap.dart';
-export 'src/popup_controller.dart';
-export 'src/marker_popup.dart';
+export 'src/popup_animation.dart';
 export 'src/popup_builder.dart';
+export 'src/popup_container/animated_popup_container.dart';
+export 'src/popup_container/simple_popup_container.dart';
+export 'src/popup_controller.dart';
+export 'src/popup_layer.dart';
+export 'src/popup_snap.dart';
