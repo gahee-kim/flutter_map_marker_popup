f79fdc70181ec4ca8b085e568d69e6caf36cf334
gahee-kim <85275890+gahee-kim@users.noreply.github.com>
2021-10-10 18:05:51 +0900

    Update flutter_map_marker_popup.dart

--- lib/flutter_map_marker_popup.dart
+++ lib/flutter_map_marker_popup.dart
@@ -4,3 +4,4 @@ export 'src/popup_marker_layer_options.dart';
 export 'src/popup_snap.dart';
 export 'src/popup_controller.dart';
 export 'src/popup_marker_layer_plugin.dart';
+export 'src/popup_animation.dart';
